254dddcd62e3bbf126f292f5538f979e8414693c
Kirill Gordine <gordine@q-one.com>
2011-10-05 12:00:37 +0200

    pullrequest.sh nun in mehreren Zeilen, v2-API

--- pullrequest.sh
+++ pullrequest.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-curl -d "pull[base]=master" -d "pull[heade]=kleidersack:feature" -d "pull[title]=feature 1" -d "pull[body]=some nice things implemented" -u "Kleidersack/token:$kleidersackpw" https://github.com/api/v2/json/pulls/gordine/Rebase-Test
+AUTH="--user Kleidersack:$kleidersackpw"
+echo $AUTH
+PARM='-d "pull[base]=master" \
+      -d "pull[heade]=kleidersack:feature" \
+      -d "pull[title]=feature 1" \
+      -d "pull[body]=some nice things implemented"'
+URL=https://github.com/api/v2/json/pulls/gordine/Rebase-Test
+CMD="curl $AUTH $PARAM $URL"
+echo $CMD
+$CMD
